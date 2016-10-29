6548e4906a11380db2a4978deb7117e58f593563
PV <arbitrary.consonence@gmail.com>
2016-10-29 19:21:06 +0530

    Demo cleanup

--- src/main/java/com/adroitandroid/DemoApplication.java
+++ src/main/java/com/adroitandroid/DemoApplication.java
@@ -1,23 +1,12 @@
 package com.adroitandroid;
 
-import com.adroitandroid.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.HashMap;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static HashMap<Long, Student> hmStudent;
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-
-		hmStudent= new HashMap<>();
-		Student one=new Student(1, "John","math");
-		hmStudent.put(one.getId(),one);
-		Student two=new Student(2, "Jane","history");
-		hmStudent.put(two.getId(),two);
 	}
 }
