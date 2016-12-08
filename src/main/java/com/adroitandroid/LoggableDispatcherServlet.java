2b7c47b2795b2a69031904cb216ac779ca1310e9
PV <arbitrary.consonence@gmail.com>
2016-12-08 21:06:40 +0530

    Fixing logger issue

--- src/main/java/com/adroitandroid/LoggableDispatcherServlet.java
+++ src/main/java/com/adroitandroid/LoggableDispatcherServlet.java
@@ -4,14 +4,13 @@ import com.google.gson.JsonObject;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.HandlerExecutionChain;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
-import org.springframework.web.util.WebUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 /**
  * Created by pv on 08/12/16.
@@ -22,35 +21,36 @@ public class LoggableDispatcherServlet extends DispatcherServlet {
 
     @Override
     protected void doDispatch(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        if (!(request instanceof ContentCachingRequestWrapper)) {
-            request = new ContentCachingRequestWrapper(request);
-        }
-        if (!(response instanceof ContentCachingResponseWrapper)) {
-            response = new ContentCachingResponseWrapper(response);
-        }
 
         long startTime = System.currentTimeMillis();
         try {
             super.doDispatch(request, response);
         } finally {
-            log(request, response, System.currentTimeMillis() - startTime);
-            updateResponse(response);
+            log(new ContentCachingRequestWrapper(request), new ContentCachingResponseWrapper(response),
+                    System.currentTimeMillis() - startTime);
         }
     }
 
     private void log(HttpServletRequest requestToCache, HttpServletResponse responseToCache, long timeTaken) {
+        int status = responseToCache.getStatus();
         JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("httpStatus", responseToCache.getStatus());
+        jsonObject.addProperty("httpStatus", status);
         jsonObject.addProperty("path", requestToCache.getRequestURI());
         jsonObject.addProperty("httpMethod", requestToCache.getMethod());
         jsonObject.addProperty("timeTakenMs", timeTaken);
         jsonObject.addProperty("clientIP", requestToCache.getRemoteAddr());
+        if (status > 299) {
+            String requestBody = null;
+            try {
+                requestBody = requestToCache.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            jsonObject.addProperty("requestBody", requestBody);
+            jsonObject.addProperty("requestParams", requestToCache.getQueryString());
+            jsonObject.addProperty("tokenExpiringHeader",
+                    responseToCache.getHeader(ResponseHeaderModifierInterceptor.HEADER_TOKEN_EXPIRING));
+        }
         logger.info(jsonObject);
-    }
-
-    private void updateResponse(HttpServletResponse response) throws IOException {
-        ContentCachingResponseWrapper responseWrapper =
-                WebUtils.getNativeResponse(response, ContentCachingResponseWrapper.class);
-        responseWrapper.copyBodyToResponse();
     }
 }
